4a44bbc13fb1da498722df882212415be270bdee
wilchr0721 <113143652+wilchr0721@users.noreply.github.com>
2023-03-08 15:24:25 +0100

    definering av program minet tillagd

---
+++ program_memory.hpp
@@ -0,0 +1,9 @@
+#ifndef PROGRAM_MEMORY_H_
+#define PROGRAM_MEMORY_H_
+
+#define PROGRAM_MEMORY_DATA_SIZE 48 // 8 bitar op_code + 8 bitar op1 + 32 bitar op2.
+#define PROGRAM_MEMORY_ADDRES_SIZE 255 // det här värdet kan ju vara störe.
+
+
+
+#endif PROGRAM_MEMORY_H_
