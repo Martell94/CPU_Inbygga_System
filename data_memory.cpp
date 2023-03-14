dd9192a4bbed12c4d87551014a629884ea583ac0
wilchr0721 <113143652+wilchr0721@users.noreply.github.com>
2023-03-14 12:05:26 +0100

    data_memory funtioner tillagda

---
+++ data_memory.cpp
@@ -0,0 +1,44 @@
+
+ #include "data_memory.hpp" 
+
+/********************************************************************************
+ *
+ *  data_memory_reset: clearar data minnet
+ * 
+********************************************************************************/
+void data_memory_reset(void)
+{
+   for (uint16_t i = 0; i < DATA_MEMORY_DATA_WIDTH; i++)
+   {
+      data_memory[i] = 0x00;
+   }
+   return;
+}
+
+/********************************************************************************
+ *
+ *  data_memory_write: Skriver ett värde till en addres i dataminnet.
+ *    
+ *                   data: värdet som skall skrivas till minnesadressen.
+ *                   addres: addresen i dataminnet dit värdet skall skrivars. 
+ * 
+********************************************************************************/
+void data_memory_write(const uint32_t data,const uint16_t addres)
+{
+   if (addres <= DATA_MEMORY_DATA_WIDTH) return;
+   data_memory[addres] = data;
+   return
+}
+
+/********************************************************************************
+ *
+ *  data_memory_read: läser ett värde från en addres i dataminnet.
+ *    
+ *                 addres: addresen i dataminnet där ett värde skall läsas. 
+ * 
+********************************************************************************/
+uint32_t data_memory_read(const uint16_t addres)
+{
+   if (addres <= DATA_MEMORY_DATA_WIDTH) return 0;
+   return data_memory[addres];
+}
---
+++ data_memory.hpp
@@ -0,0 +1,38 @@
+
+#ifndef DATA_MEMORY_H_
+#define  DATA_MEMORY_H_
+
+#include <stdint.h>
+
+#define DATA_MEMORY_DATA_WIDTH  32   /* 32 bits per address. */
+#define DATA_MEMORY_ADDRESS_WIDTH 5000 /*  addresses (0 - 5000). */
+
+static uint32_t data_memory[DATA_MEMORY_ADDRESS_WIDTH];
+
+/********************************************************************************
+ *
+ *  data_memory_reset: clearar data minnet
+ * 
+********************************************************************************/
+void data_memory_reset(void);
+
+/********************************************************************************
+ *
+ *  data_memory_write: Skriver ett värde till en addres i dataminnet.
+ *    
+ *                   data: värdet som skall skrivas till minnesadressen.
+ *                   addres: addresen i dataminnet dit värdet skall skrivars. 
+ * 
+********************************************************************************/
+void data_memory_write(const uint32_t data,const uint16_t addres);
+
+/********************************************************************************
+ *
+ *  data_memory_read: läser ett värde från en addres i dataminnet.
+ *    
+ *                 addres: addresen i dataminnet där ett värde skall läsas. 
+ * 
+********************************************************************************/
+uint32_t data_memory_read(const uint16_t addres);
+
+#endif //DATA_MEMORY_H_
