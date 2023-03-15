b2e964d4e2f2d2c3fbdc6801b8577c1e8b033d59
Sebastian Jansson <sebjan0413@skola.goteborg.se>
2023-03-15 15:09:32 +0100

    La till statiska funktioner i programminnet

--- program_memory.hpp
+++ program_memory.hpp
@@ -4,6 +4,10 @@
 #define PROGRAM_MEMORY_DATA_SIZE 48 // 8 bitar op_code + 8 bitar op1 + 32 bitar op2.
 #define PROGRAM_MEMORY_ADDRES_SIZE 255 // det här värdet kan ju vara störe.
 
+/* Statiska funktioner: */
 
+static inline uint8_t op_code;
+static inline uint8_t op1;
+static inline uint32_t op2;
 
 #endif PROGRAM_MEMORY_H_
